7d3c02347fd3495cebd4f94234be0347f2bf17fc
Surendar Yadav <skysurendar@gmail.com>
2023-12-10 20:27:40 +0530

    Delete MeschinoWellness directory

--- MeschinoWellness/src/providers/index.ts
+++
@@ -1,7 +0,0 @@
-export { Api } from './api/api';
-export { Items } from '../mocks/providers/items';
-export { Settings } from './settings/settings';
-export { HraService } from './hra/hra';
-export { NotificationService } from './user/notification';
-export { StepChallengeService } from './steps/stepschallenge'
-export { UserService } from './user/user';
